28d885fdb5dd920a90c9ddab29ae8ea2d76efcdc
Fernweh99 <daniele.dimento@gmail.com>
2022-07-15 15:38:24 +0200

    not completed milestone 4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-
+Vue.config.devtools = true;
 const app = new Vue({
   name: 'whatsapp',
   el: "#root",
   data: {
-    currentIndex: 0,
+    currentNameIndex: "Michele",
+    currentI: 0,
     currentTextMessage: "",
     textSearch: "",
+    searchContacts: [], 
     user: {
       name: 'Nome Utente',
       avatar: '_io'
@@ -29,12 +31,12 @@ const app = new Vue({
           text: 'Tutto fatto!',
           status: 'received'
         }
-      ],
-    },
-    {
-      name: 'Fabio',
-      avatar: '_2',
-      messages: [{
+        ],
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        messages: [{
         date: '20/03/2020 16:30:00',
         text: 'Ciao come stai?',
         status: 'sent'
@@ -49,32 +51,32 @@ const app = new Vue({
         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
         status: 'received'
       }
-    ],
-  },
-  {
-    name: 'Samuele',
-    avatar: '_3',
-    messages: [{
-      date: '28/03/2020 10:10:40',
-      text: 'La Marianna va in campagna',
-      status: 'received'
-    },
-    {
-      date: '28/03/2020 10:20:10',
-      text: 'Sicuro di non aver sbagliato chat?',
-      status: 'sent'
-    },
-    {
-      date: '28/03/2020 16:15:22',
-      text: 'Ah scusa!',
-      status: 'received'
-    }
-  ],
-},
-{
-  name: 'Luisa',
-  avatar: '_4',
-  messages: [{
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        messages: [{
+        date: '28/03/2020 10:10:40',
+        text: 'La Marianna va in campagna',
+        status: 'received'
+      },
+      {
+        date: '28/03/2020 10:20:10',
+        text: 'Sicuro di non aver sbagliato chat?',
+        status: 'sent'
+      },
+      {
+        date: '28/03/2020 16:15:22',
+        text: 'Ah scusa!',
+        status: 'received'
+      }
+        ],
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+        messages: [{
     date: '10/01/2020 15:30:55',
     text: 'Lo sai che ha aperto una nuova pizzeria?',
     status: 'sent'
@@ -84,24 +86,34 @@ const app = new Vue({
     text: 'Si, ma preferirei andare al cinema',
     status: 'received'
   }
-],
-},
-],
-},
-computed: {
-},
-methods: {
-  sendAndReceiveMessage() {
-    const mex = this.contacts[this.currentIndex].messages; 
-    mex.push({date: this.getTimeNow(), text: this.currentTextMessage, status: 'sent'});
-    this.currentTextMessage = "";
-    setTimeout(()=>{
-      mex.push({date: this.getTimeNow(), text: 'ok', status: 'received'});
-    },1000)
+        ],
+      },
+    ],
   },
-  getTimeNow() {
-    const date = new Date()
-    return date.toLocaleString();
+  computed: {
+    getContactsSearch() {
+      this.searchContacts = this.contacts.filter(contact => {
+        const firstname = contact.name.toLowerCase();
+        const search = this.textSearch.toLowerCase();
+        return (firstname.includes(search)) 
+      })
+    },
+  },
+  methods: {
+    sendAndReceiveMessage() {
+      const index = this.contacts.findIndex(contact =>{
+        return contact.name === this.currentNameIndex;
+      }) 
+      const mex = this.contacts[index].messages; 
+      mex.push({date: this.getTimeNow(), text: this.currentTextMessage, status: 'sent'});
+      this.currentTextMessage = "";
+      setTimeout(()=>{
+        mex.push({date: this.getTimeNow(), text: 'ok', status: 'received'});
+      },1000)
+    },
+    getTimeNow() {
+      const date = new Date()
+      return date.toLocaleString();
+    },
   }
-}
 })
