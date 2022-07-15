1daf9e420ba197111a4a4ea658da2a9617a0f3b7
Fernweh99 <daniele.dimento@gmail.com>
2022-07-15 12:23:50 +0200

    add array messages and fixed, add date at new mex

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+
 const app = new Vue({
   name: 'whatsapp',
   el: "#root",
   data: {
     currentIndex: 0,
     currentTextMessage: "",
+    textSearch: "",
     user: {
       name: 'Nome Utente',
       avatar: '_io'
@@ -12,85 +14,94 @@ const app = new Vue({
       {
         name: 'Michele',
         avatar: '_1',
-        messages: [
-          {
-            name: 'User',
-            text: 'Hai portato a spasso il cane?',
-          },
-          {
-            name: 'User',
-            text: 'Ricordati di dargli da mangiare',
-          },
-          {
-            name: 'Michele',
-            text: 'Tutto Fatto!',
-          },
-        ]
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          text: 'Hai portato a spasso il cane?',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          text: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 16:15:22',
+          text: 'Tutto fatto!',
+          status: 'received'
+        }
+      ],
+    },
+    {
+      name: 'Fabio',
+      avatar: '_2',
+      messages: [{
+        date: '20/03/2020 16:30:00',
+        text: 'Ciao come stai?',
+        status: 'sent'
       },
       {
-        name: 'Fabio',
-        avatar: '_2',
-        messages: [
-          {
-            name: 'User',
-            text: 'Hai portato a spasso il cane?',
-          },
-          {
-            name: 'Fabio',
-            text: 'Ricordati di dargli da mangiare',
-          },
-          {
-            name: 'Fabio',
-            text: 'Ricordati di dargli da mangiare',
-          },
-          {
-            name: 'User',
-            text: 'Tutto Fatto!',
-          },
-        ]
+        date: '20/03/2020 16:30:55',
+        text: 'Bene grazie! Stasera ci vediamo?',
+        status: 'received'
       },
       {
-        name: 'Samuele',
-        avatar: '_3',
-        messages: [
-          {
-            name: 'Samuele',
-            text: 'Hai portato a spasso il cane?',
-          },
-          {
-            name: 'User',
-            text: 'Ricordati di dargli da mangiare',
-          },
-          {
-            name: 'Samuele',
-            text: 'Tutto Fatto!',
-          },
-        ]
-      },
-      {
-        name: 'Luisa',
-        avatar: '_4',
-        messages: [
-          {
-            name: 'User',
-            text: 'Hai portato a spasso il cane?',
-          },
-          {
-            name: 'Luisa',
-            text: 'Ricordati di dargli da mangiare',
-          },
-        ]
-      },
+        date: '20/03/2020 16:35:00',
+        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+        status: 'received'
+      }
     ],
   },
-  methods: {
-    sendAndReceiveMessage() {
-      const mex = this.contacts[this.currentIndex].messages; 
-      mex.push({name: 'User', text: this.currentTextMessage});
-      this.currentTextMessage = "";
-      setTimeout(()=>{
-        mex.push({name: this.contacts[this.currentIndex].name, text: 'ok'})
-      },1000)
+  {
+    name: 'Samuele',
+    avatar: '_3',
+    messages: [{
+      date: '28/03/2020 10:10:40',
+      text: 'La Marianna va in campagna',
+      status: 'received'
     },
+    {
+      date: '28/03/2020 10:20:10',
+      text: 'Sicuro di non aver sbagliato chat?',
+      status: 'sent'
+    },
+    {
+      date: '28/03/2020 16:15:22',
+      text: 'Ah scusa!',
+      status: 'received'
+    }
+  ],
+},
+{
+  name: 'Luisa',
+  avatar: '_4',
+  messages: [{
+    date: '10/01/2020 15:30:55',
+    text: 'Lo sai che ha aperto una nuova pizzeria?',
+    status: 'sent'
+  },
+  {
+    date: '10/01/2020 15:50:00',
+    text: 'Si, ma preferirei andare al cinema',
+    status: 'received'
   }
+],
+},
+],
+},
+computed: {
+},
+methods: {
+  sendAndReceiveMessage() {
+    const mex = this.contacts[this.currentIndex].messages; 
+    mex.push({date: this.getTimeNow(), text: this.currentTextMessage, status: 'sent'});
+    this.currentTextMessage = "";
+    setTimeout(()=>{
+      mex.push({date: this.getTimeNow(), text: 'ok', status: 'received'});
+    },1000)
+  },
+  getTimeNow() {
+    const date = new Date()
+    return date.toLocaleString();
+  }
+}
 })
